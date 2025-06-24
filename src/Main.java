1611e9c112b8d6ffe355915862a5d3092f975478
whals <whalsdud0131@gmail.cm>
2025-06-24 13:09:57 +0900

    File IO

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void writeFile(String filename,String content) {
+        try (FileWriter writer = new FileWriter(filename, true)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void readFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //writeFile("lunch-menu.txt","\n제육볶음");
+        //System.out.println("파일 생성 완료");
+
+        readFile("lunch-menu.txt");
     }
 }
